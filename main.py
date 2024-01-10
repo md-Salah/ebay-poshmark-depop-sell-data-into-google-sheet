2fb74a557a91e1bad200be94bb55cb0a6c74b8f7
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-01-10 23:37:09 +0600

    selective cell update

--- main.py
+++ main.py
@@ -4,52 +4,53 @@ from collections import Counter
 
 from modules.settings import Settings
 from modules.depop import Depop
-from modules.poshmark import Poshmark   
+from modules.poshmark import Poshmark
 from modules.ebay import Ebay
 from modules.gsheet import GoogleSheet
+
 
 def main():
     start_time = time.time()
     settings = Settings()
-    
+
     try:
         sold_items = []
-        
-        # # Ebay
-        # ebay = Ebay(profile=settings.profile)
-        # if ebay.login(settings.ebay_username, settings.ebay_password):
-        #     sold_items += ebay.get_sold_items()
-        # del ebay
-        # time.sleep(3)
-        
-        
-        # # Poshmark
-        # poshmark = Poshmark(profile=settings.profile)
-        # if poshmark.login(settings.poshmark_username, settings.poshmark_password):
-        #     sold_items += poshmark.get_sold_items()
-        # del poshmark
-        # time.sleep(3)
-        
-        
-        # # Depop
-        # depop = Depop(profile=settings.profile)
-        # if depop.login(settings.depop_username, settings.depop_password):
-        #     sold_items += depop.get_sold_items()
-        # del depop
-            
-        
+
+        # Ebay
+        ebay = Ebay(profile=settings.profile)
+        if ebay.login(settings.ebay_username, settings.ebay_password):
+            sold_items += ebay.get_sold_items()
+        del ebay
+        time.sleep(3)
+
+        # Poshmark
+        poshmark = Poshmark(profile=settings.profile)
+        if poshmark.login(settings.poshmark_username, settings.poshmark_password):
+            sold_items += poshmark.get_sold_items()
+        del poshmark
+        time.sleep(3)
+
+        # Depop
+        depop = Depop(profile=settings.profile)
+        if depop.login(settings.depop_username, settings.depop_password):
+            sold_items += depop.get_sold_items()
+        del depop
+
         print('\nTotal sold items: {}'.format(len(sold_items)))
-        [print('{}: {}'.format(key, value)) for key, value in Counter([item.get('platform') for item in sold_items]).items()]
-        
+        [print('{}: {}'.format(key, value)) for key, value in Counter(
+            [item.get('platform') for item in sold_items]).items()]
+
         # Google Sheet
         print('\nUpdating Google Sheet...')
-        gsheet = GoogleSheet(service_account=settings.service_account) # type: ignore
-        gsheet.update_sold_items(filename=settings.filename, sheetname=settings.sheetname, sold_items=sold_items) # type: ignore
+        gsheet = GoogleSheet(
+            service_account=settings.service_account)  # type: ignore
+        gsheet.update_sold_items(
+            filename=settings.filename, sheetname=settings.sheetname, sold_items=sold_items)  # type: ignore
         del gsheet
-        
+
         print('')
         [print(item) for item in sold_items]
-        
+
         # input('\nPress any key to exit...')
     except Exception:
         print('Unexpected error occurred. Please see "error.log" for more details.')
@@ -59,10 +60,6 @@ def main():
         print('\nExecution time: {} min'.format(
             round((time.time() - start_time)/60, 2)))
 
-        
 
 if __name__ == '__main__':
     main()
-    
-    
-    
--- modules/gsheet.py
+++ modules/gsheet.py
@@ -12,36 +12,10 @@ class GoogleSheet:
             self.sa = gspread.service_account(filename=service_account)  # type: ignore
         except Exception:
             traceback.print_exc()
-    
-    def read_sheet(self, filename:str, sheetname:str) -> list[dict]:
-        try:
-            sheets = self.sa.open(filename)
-            wsh = sheets.worksheet(sheetname)
-            
-            return wsh.get_all_records()  
-        except (SpreadsheetNotFound, ConnectionError):
-            print('SpreadsheetNotFound: {} + {}'.format(filename, sheetname))
-        
-        return []   
-        
-        
-    def update_sheet(self, filename:str, sheetname:str, data:list[dict], columns=[]):
-        try:
-            sheets = self.sa.open(filename)
-            wsh = sheets.worksheet(sheetname)
-            
-            df = pd.DataFrame(data)
-            df = df[columns] if columns else df
-            df = df.fillna('')
-            
-            wsh.clear()
-            wsh.update([df.columns.values.tolist()] + df.values.tolist())
-        except SpreadsheetNotFound:
-            print('Spreadsheet not found: {} + {}'.format(filename, sheetname))
-            os.system('pause')
-        
         
     def update_sold_items(self, filename:str, sheetname:str, sold_items:list[dict]):
+        added, updated = [], []
+
         # Strip header
         sheets = self.sa.open(filename)
         wsh = sheets.worksheet(sheetname)
@@ -56,26 +30,29 @@ class GoogleSheet:
             'Sold Date': 'date',
             'Sold Platform': 'platform'
         }
-        added, updated = [], []
         
         for item in sold_items:
             index = self.item_in_products(item['item_code'], products)
             if index == -1:
                 # Add new item
-                new_product = {key:item.get(col_map.get(key, ''), '') for key in header}
-                new_product['Name'] = item['name']
-                products.append(new_product)
+                col_map['Name'] = 'name'
+                row = len(products) + 2
+                for key in col_map:
+                    value = item.get(col_map[key], '')
+                    wsh.update_cell(row, header.index(key)+1, value)
+                
                 added.append(item['item_code'])
             else:
                 # Update existing item
+                row = index + 2
                 for key in col_map:
-                    products[index][key] = item[col_map[key]]
+                    value = item.get(col_map[key], '')
+                    wsh.update_cell(row, header.index(key)+1, value)
                 updated.append(item['item_code'])
 
-        # Save the changes
-        self.update_sheet(filename, sheetname, products, header)
         print('Added: {}'.format(added))
         print('Updated: {}'.format(updated))
+            
             
     def item_in_products(self, item_code:str, products:list[dict]) -> int:
         item_code = str(item_code).strip()
@@ -83,6 +60,5 @@ class GoogleSheet:
         for index, product in enumerate(products):
             if item_code in str(product['Name']):
                 return index
-        
         return -1
     
